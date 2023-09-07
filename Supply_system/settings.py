e3f3735367d717527ec4ec45481ccc39c9a38e75
JELYN ABRIO <jelynabrio700@gmail.com>
2023-09-07 13:18:38 +0800

    1pm

--- Supply_system/settings.py
+++ Supply_system/settings.py
@@ -75,21 +75,21 @@ WSGI_APPLICATION = 'Supply_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
- #   }
-#}
-
 DATABASES = {
-'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME':  'mydatabase',
-       'HOST': 'localhost',
-       'PORT': 27017,
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+#DATABASES = {
+#'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME':  'mydatabase',
+#       'HOST': 'localhost',
+#       'PORT': 27017,
+#     }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
